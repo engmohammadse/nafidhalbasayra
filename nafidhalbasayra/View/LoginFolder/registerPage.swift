6301571e1ae75d94dd605edc82708b8317d9997f
mohammad <mohammadalaaraji@gmail.com>
2024-10-01 19:31:15 +0300

    Update RegisterPage Add DatePicker

--- nafidhalbasayra/View/LoginFolder/registerPage.swift
+++ nafidhalbasayra/View/LoginFolder/registerPage.swift
@@ -249,7 +249,7 @@ struct registerPage: View {
     @State private var name: String = ""
     @State private var birthDay: String = ""
     @State private var phoneNumber: String = ""
-    
+    @State private var isNavigate: Bool = false
     @State private var selectedDate: Date = Date() // The date the user selects
     @State private var isPickerVisible: Bool = false // Controls visibility of the picker
     
@@ -266,7 +266,7 @@ struct registerPage: View {
                     .font(.custom("BahijTheSansArabic-Bold", size: UIDevice.current.userInterfaceIdiom == .phone ? geoW * 0.035 : geoW * 0.02))
                     .padding(.trailing, UIDevice.current.userInterfaceIdiom == .pad ? geoW * 0.2 : geoW * 0.05)
                 
-                TextField("user1          ", text: $name)
+                TextField("", text: $name)
                     .frame(maxWidth: geoH * 0.4)
                     .frame(maxHeight: geoH * 0.05)
                     .multilineTextAlignment(.trailing)
@@ -302,8 +302,9 @@ struct registerPage: View {
                             Image(systemName: "calendar")
                                 .foregroundColor(.blue)
                                 .padding(.trailing, 10)
-                        }
+                        }.offset(x: UIDevice.current.userInterfaceIdiom == .phone ? geoW * -0.3 : geoW * -0.2)
                     }
+                      
                     
                     // Calendar icon button
                   
@@ -318,7 +319,7 @@ struct registerPage: View {
                     .font(.custom("BahijTheSansArabic-Bold", size: UIDevice.current.userInterfaceIdiom == .phone ? geoW * 0.035 : geoW * 0.02))
                     .padding(.trailing, UIDevice.current.userInterfaceIdiom == .pad ? geoW * 0.2 : geoW * 0.05)
                 
-                TextField("user1          ", text: $phoneNumber)
+                TextField("", text: $phoneNumber)
                     .frame(maxWidth: geoH * 0.4)
                     .frame(maxHeight: geoH * 0.05)
                     .multilineTextAlignment(.trailing)
@@ -334,7 +335,7 @@ struct registerPage: View {
                     .font(.custom("BahijTheSansArabic-Bold", size: UIDevice.current.userInterfaceIdiom == .phone ? geoW * 0.032 : geoW * 0.02))
                     .padding(.trailing, UIDevice.current.userInterfaceIdiom == .pad ? geoW * 0.2 : geoW * 0.05)
                 
-                TextField("user1          ", text: $phoneNumber)
+                TextField("", text: $phoneNumber)
                     .frame(maxWidth: geoH * 0.4)
                     .frame(maxHeight: geoH * 0.05)
                     .multilineTextAlignment(.trailing)
@@ -350,7 +351,7 @@ struct registerPage: View {
                     .font(.custom("BahijTheSansArabic-Bold", size: UIDevice.current.userInterfaceIdiom == .phone ? geoW * 0.032 : geoW * 0.02))
                     .padding(.trailing, UIDevice.current.userInterfaceIdiom == .pad ? geoW * 0.2 : geoW * 0.05)
                 
-                TextField("user1          ", text: $phoneNumber)
+                TextField("", text: $phoneNumber)
                     .frame(maxWidth: geoH * 0.4)
                     .frame(maxHeight: geoH * 0.05)
                     .multilineTextAlignment(.trailing)
@@ -360,41 +361,85 @@ struct registerPage: View {
                 
                 Spacer().frame(maxHeight: geoH * 0.01)
                 
+          
+                    
+                
+                        Text("رمز المحافظة")
+                            .alignmentGuide(.leading) { d in d[.trailing] } // Custom alignment
+                            .frame(maxWidth: .infinity, alignment: .trailing) // Push
+                            .font(.custom("BahijTheSansArabic-Bold", size: UIDevice.current.userInterfaceIdiom == .phone ? geoW * 0.032 : geoW * 0.02))
+                            .padding(.trailing, UIDevice.current.userInterfaceIdiom == .pad ? geoW * 0.2 : geoW * 0.05)
+                        
+                        TextField("", text: $phoneNumber)
+                            .frame(maxWidth: geoH * 0.4)
+                            .frame(maxHeight: geoH * 0.05)
+                            .multilineTextAlignment(.trailing)
+                            .padding(.horizontal)
+                            .background(Color.white)
+                            .cornerRadius(5)
+                
+                
+                
                 HStack {
-                    VStack(spacing: 10) {
-                        Text("القضاء")
-                            .alignmentGuide(.leading) { d in d[.trailing] } // Custom alignment
-                            .frame(maxWidth: .infinity, alignment: .trailing) // Push
-                            .font(.custom("BahijTheSansArabic-Bold", size: UIDevice.current.userInterfaceIdiom == .phone ? geoW * 0.032 : geoW * 0.02))
-                            .padding(.trailing, UIDevice.current.userInterfaceIdiom == .pad ? geoW * 0.2 : geoW * 0.05)
+                    
+                    Image("Group 9")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(maxWidth: geoW * 0.05)
+                        .padding(.vertical, geoH * 0.01)
+                        .padding(.leading, UIDevice.current.userInterfaceIdiom == .phone ? geoW * 0.05: geoW * 0.025)
+                    
+                    
+                    
+                    Button(action: {
+                        // Action for previous button
+                    }) {
+                        Text("السابق")
+                            .font(.custom("BahijTheSansArabic-Bold", size: UIDevice.current.userInterfaceIdiom == .phone ? geoW * 0.04 : geoW * 0.03))
+                            .foregroundStyle(Color(UIColor(red: 90 / 255, green: 97 / 255, blue: 103 / 255, alpha: 1.0))
+)
+                    }
+                    .padding(.horizontal, geoW * 0.005)
+                   // .background(Color.black.opacity(0.8))
+                    .foregroundColor(.white)
+                    .cornerRadius(10)
+                    
+                    Image("Line 1")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: geoH * 0.01 ,height: geoH * 0.04)
+                        .padding(.vertical, geoH * 0.01)
+                    
+                    Button(action: {
+                        // Action for next button
                         
-                        TextField("user1          ", text: $phoneNumber)
-                            .frame(maxWidth: geoH * 0.4)
-                            .frame(maxHeight: geoH * 0.05)
-                            .multilineTextAlignment(.trailing)
-                            .padding(.horizontal)
-                            .background(Color.white)
-                            .cornerRadius(5)
+                        isNavigate = true
+                    }) {
+                        Text("التالي")
+                            .font(.custom("BahijTheSansArabic-Bold", size: UIDevice.current.userInterfaceIdiom == .phone ? geoW * 0.04 : geoW * 0.03))
+                    }
+                    .padding(.horizontal, geoW * 0.005)
+                   // .background(Color.black.opacity(0.8))
+                    .foregroundColor(.white)
+                    .cornerRadius(10)
+                    .navigationDestination(isPresented: $isNavigate) {
+                        registerPage()
                     }
                     
-                    Spacer().frame(maxWidth: geoW * 0.05)
                     
-                    VStack(spacing: 10) {
-                        Text("المحافظة")
-                            .alignmentGuide(.leading) { d in d[.trailing] } // Custom alignment
-                            .frame(maxWidth: .infinity, alignment: .trailing) // Push
-                            .font(.custom("BahijTheSansArabic-Bold", size: UIDevice.current.userInterfaceIdiom == .phone ? geoW * 0.032 : geoW * 0.02))
-                            .padding(.trailing, UIDevice.current.userInterfaceIdiom == .pad ? geoW * 0.2 : geoW * 0.05)
-                        
-                        TextField("user1          ", text: $phoneNumber)
-                            .frame(maxWidth: geoH * 0.4)
-                            .frame(maxHeight: geoH * 0.05)
-                            .multilineTextAlignment(.trailing)
-                            .padding(.horizontal)
-                            .background(Color.white)
-                            .cornerRadius(5)
-                    }
-                }
+                    Image("Group 16")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(maxWidth: geoW * 0.05)
+                        .padding(.vertical, geoH * 0.01)
+                        .padding(.trailing, UIDevice.current.userInterfaceIdiom == .phone ? geoW * 0.05: geoW * 0.025 )
+                    
+                    
+                    
+                }.background(Color(UIColor(red: 20 / 255, green: 30 / 255, blue: 39 / 255, alpha: 1.0)))
+                    .cornerRadius(5)
+                    .padding()
+                
             }
             .padding(.horizontal, 40)
             .frame(maxWidth: .infinity, maxHeight: .infinity)
