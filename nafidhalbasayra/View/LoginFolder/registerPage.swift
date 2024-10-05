7510e6a15fb2992e2c8a4dd44d8c0952679ced34
mohammad <mohammadalaaraji@gmail.com>
2024-10-05 17:47:34 +0300

    Update Fix keyboard show issue containt  jump to up in registerPage

--- nafidhalbasayra/View/LoginFolder/registerPage.swift
+++ nafidhalbasayra/View/LoginFolder/registerPage.swift
@@ -14,176 +14,207 @@ struct registerPage: View {
     @State private var birthDay: String = ""
     @State private var phoneNumber: String = ""
     @State private var isNavigate: Bool = false
-    @State private var selectedDate: Date = Date() // The date the user selects
-    @State private var isPickerVisible: Bool = false // Controls visibility of the picker
-    @State private var keyboardHeight: CGFloat = 0 // To track the keyboard height
-    @FocusState private var activeField: Field? // Focus state for TextFields
+    @State private var selectedDate: Date = Date()
+    @State private var isPickerVisible: Bool = false
+    @State private var keyboardHeight: CGFloat = 0
+    @FocusState private var activeField: Field?
     @State private var academicLevel: String = ""
     @State private var currentWork: String = ""
     @State private var cityNumber: String = ""
-    
+
     enum Field {
         case academicLevel, currentWork, cityNumber
     }
     
     var body: some View {
         
-        ScrollView {
-            VStack(spacing: 10) {
-                
-                Spacer().frame(height: UIDevice.current.userInterfaceIdiom == .phone ? screenHeight * 0.07 : screenHeight * 0.10)
-                
-                // Field: Name
-                Text("الأسم الرباعي واللقب")
-                    .alignmentGuide(.leading) { d in d[.trailing] } // Custom alignment
-                    .frame(maxWidth: .infinity, alignment: .trailing) // Push
-                    .font(.custom("BahijTheSansArabic-Bold", size: UIDevice.current.userInterfaceIdiom == .phone ? screenWidth * 0.035 : screenWidth * 0.02))
-                    .padding(.trailing, UIDevice.current.userInterfaceIdiom == .pad ? screenWidth * 0.2 : screenWidth * 0.05)
-                
-                TextField("", text: $name)
+        
+        
+//        .overlay{
+//        
+//        PreviousNextButton(previousAction: {}, nextAction: {}, geoW: screenWidth, geoH: screenHeight, isNextNavigating: true, isPreviosNavigating: true, destination: AnyView(EmptyView()), destinationBack: AnyView(LoginPageWelcom()) , color: Color.white, imageName: "Group 9")
+//    }
+//    .offset(y: screenHeight * 0.02)
+        
+        ZStack {
+            
+            
+            ScrollView {
+                VStack(spacing: 10) {
+                    
+                    Spacer().frame(height: UIDevice.current.userInterfaceIdiom == .phone ? screenHeight * 0.07 : screenHeight * 0.10)
+                    
+                    // Field: Name
+                    Text("الأسم الرباعي واللقب")
+                        .alignmentGuide(.leading) { d in d[.trailing] }
+                        .frame(maxWidth: .infinity, alignment: .trailing)
+                        .font(.custom("BahijTheSansArabic-Bold", size: UIDevice.current.userInterfaceIdiom == .phone ? screenWidth * 0.035 : screenWidth * 0.02))
+                        .padding(.trailing, UIDevice.current.userInterfaceIdiom == .pad ? screenWidth * 0.2 : screenWidth * 0.05)
+                    
+                    TextField("", text: $name)
+                        .frame(maxWidth: screenHeight * 0.4)
+                        .frame(height: screenHeight * 0.05)
+                        .multilineTextAlignment(.trailing)
+                        .padding(.horizontal)
+                        .background(Color.white)
+                        .cornerRadius(5)
+                    
+                    Spacer().frame(maxHeight: screenHeight * 0.01)
+                    
+                    // Field: Birth Day
+                    Text("تاريخ الولادة")
+                        .alignmentGuide(.leading) { d in d[.trailing] }
+                        .frame(maxWidth: .infinity, alignment: .trailing)
+                        .font(.custom("BahijTheSansArabic-Bold", size: UIDevice.current.userInterfaceIdiom == .phone ? screenWidth * 0.035 : screenWidth * 0.02))
+                        .padding(.trailing, UIDevice.current.userInterfaceIdiom == .pad ? screenWidth * 0.2 : screenWidth * 0.05)
+                    
+                    TextField("Select your birthday", text: Binding(
+                        get: { formattedDate(selectedDate) },
+                        set: { _ in }
+                    ))
+                    .disabled(true)
                     .frame(maxWidth: screenHeight * 0.4)
                     .frame(height: screenHeight * 0.05)
                     .multilineTextAlignment(.trailing)
                     .padding(.horizontal)
                     .background(Color.white)
                     .cornerRadius(5)
-                
-                Spacer().frame(maxHeight: screenHeight * 0.01)
-                
-                // Field: Birth Day
-                Text("تاريخ الولادة")
-                    .alignmentGuide(.leading) { d in d[.trailing] } // Custom alignment
-                    .frame(maxWidth: .infinity, alignment: .trailing) // Push
-                    .font(.custom("BahijTheSansArabic-Bold", size: UIDevice.current.userInterfaceIdiom == .phone ? screenWidth * 0.035 : screenWidth * 0.02))
-                    .padding(.trailing, UIDevice.current.userInterfaceIdiom == .pad ? screenWidth * 0.2 : screenWidth * 0.05)
-                
-                TextField("Select your birthday", text: Binding(
-                    get: { formattedDate(selectedDate) },
-                    set: { _ in }
-                ))
-                .disabled(true) // Disable typing in the TextField
-                .frame(maxWidth: screenHeight * 0.4)
-                .frame(height: screenHeight * 0.05)
-                .multilineTextAlignment(.trailing)
-                .padding(.horizontal)
-                .background(Color.white)
-                .cornerRadius(5)
-                .overlay{
-                    Button(action: {
-                        isPickerVisible.toggle() // Toggle the DatePicker visibility
-                    }) {
-                        Image(systemName: "calendar")
-                            .foregroundColor(.blue)
-                            .padding(.trailing, 10)
-                    }.offset(x: UIDevice.current.userInterfaceIdiom == .phone ? screenWidth * -0.3 : screenWidth * -0.2)
+                    .overlay{
+                        Button(action: {
+                            isPickerVisible.toggle()
+                        }) {
+                            Image(systemName: "calendar")
+                                .foregroundColor(.blue)
+                                .padding(.trailing, 10)
+                        }.offset(x: UIDevice.current.userInterfaceIdiom == .phone ? screenWidth * -0.3 : screenWidth * -0.2)
+                    }
+                    
+                    Spacer().frame(maxHeight: screenHeight * 0.01)
+                    
+                    // Field: Phone Number
+                    Text("رقم الهاتف الخاص بالأستاذ")
+                        .alignmentGuide(.leading) { d in d[.trailing] }
+                        .frame(maxWidth: .infinity, alignment: .trailing)
+                        .font(.custom("BahijTheSansArabic-Bold", size: UIDevice.current.userInterfaceIdiom == .phone ? screenWidth * 0.035 : screenWidth * 0.02))
+                        .padding(.trailing, UIDevice.current.userInterfaceIdiom == .pad ? screenWidth * 0.2 : screenWidth * 0.05)
+                    
+                    TextField("", text: $phoneNumber)
+                        .frame(maxWidth: screenHeight * 0.4)
+                        .frame(height: screenHeight * 0.05)
+                        .multilineTextAlignment(.trailing)
+                        .padding(.horizontal)
+                        .background(Color.white)
+                        .cornerRadius(5)
+                    
+                    Spacer().frame(maxHeight: screenHeight * 0.01)
+                    
+                    // Field: Academic Level
+                    Text("التحصيل الحوزوي او الدراسي ( مع التخصص الدقيق)")
+                        .alignmentGuide(.leading) { d in d[.trailing] }
+                        .frame(maxWidth: .infinity, alignment: .trailing)
+                        .font(.custom("BahijTheSansArabic-Bold", size: UIDevice.current.userInterfaceIdiom == .phone ? screenWidth * 0.032 : screenWidth * 0.02))
+                        .padding(.trailing, UIDevice.current.userInterfaceIdiom == .pad ? screenWidth * 0.2 : screenWidth * 0.05)
+                    
+                    TextField("", text: $academicLevel)
+                        .frame(maxWidth: screenHeight * 0.4)
+                        .frame(height: screenHeight * 0.05)
+                        .multilineTextAlignment(.trailing)
+                        .padding(.horizontal)
+                        .background(Color.white)
+                        .cornerRadius(5)
+                        .focused($activeField, equals: .academicLevel)
+                    
+                    Spacer().frame(maxHeight: screenHeight * 0.01)
+                    
+                    // Field: Current Work
+                    Text("العمل الحالي")
+                        .alignmentGuide(.leading) { d in d[.trailing] }
+                        .frame(maxWidth: .infinity, alignment: .trailing)
+                        .font(.custom("BahijTheSansArabic-Bold", size: UIDevice.current.userInterfaceIdiom == .phone ? screenWidth * 0.032 : screenWidth * 0.02))
+                        .padding(.trailing, UIDevice.current.userInterfaceIdiom == .pad ? screenWidth * 0.2 : screenWidth * 0.05)
+                    
+                    TextField("", text: $currentWork)
+                        .frame(maxWidth: screenHeight * 0.4)
+                        .frame(height: screenHeight * 0.05)
+                        .multilineTextAlignment(.trailing)
+                        .padding(.horizontal)
+                        .background(Color.white)
+                        .cornerRadius(5)
+                        .focused($activeField, equals: .currentWork)
+                    
+                    Spacer().frame(maxHeight: screenHeight * 0.01)
+                    
+                    // Field: City Number
+                    Text("رمز المحافظة")
+                        .alignmentGuide(.leading) { d in d[.trailing] }
+                        .frame(maxWidth: .infinity, alignment: .trailing)
+                        .font(.custom("BahijTheSansArabic-Bold", size: UIDevice.current.userInterfaceIdiom == .phone ? screenWidth * 0.032 : screenWidth * 0.02))
+                        .padding(.trailing, UIDevice.current.userInterfaceIdiom == .pad ? screenWidth * 0.2 : screenWidth * 0.05)
+                    
+                    TextField("", text: $cityNumber)
+                        .frame(maxWidth: screenHeight * 0.4)
+                        .frame(height: screenHeight * 0.05)
+                        .multilineTextAlignment(.trailing)
+                        .padding(.horizontal)
+                        .background(Color.white)
+                        .cornerRadius(5)
+                        .focused($activeField, equals: .cityNumber)
+                    
                 }
                 
-                Spacer().frame(maxHeight: screenHeight * 0.01)
+                .padding(.horizontal, UIScreen.main.bounds.width < 500 ? 16 : 0)
+                .offset(y: calculateOffset()) // Apply offset only for the bottom fields
+                .onAppear {
+                    self.addKeyboardObservers()
+                }
+                .onDisappear {
+                    self.removeKeyboardObservers()
+                }
                 
-                // Field: Phone Number
-                Text("رقم الهاتف الخاص بالأستاذ")
-                    .alignmentGuide(.leading) { d in d[.trailing] } // Custom alignment
-                    .frame(maxWidth: .infinity, alignment: .trailing) // Push
-                    .font(.custom("BahijTheSansArabic-Bold", size: UIDevice.current.userInterfaceIdiom == .phone ? screenWidth * 0.035 : screenWidth * 0.02))
-                    .padding(.trailing, UIDevice.current.userInterfaceIdiom == .pad ? screenWidth * 0.2 : screenWidth * 0.05)
                 
-                TextField("", text: $phoneNumber)
-                    .frame(maxWidth: screenHeight * 0.4)
-                    .frame(height: screenHeight * 0.05)
-                    .multilineTextAlignment(.trailing)
-                    .padding(.horizontal)
-                    .background(Color.white)
-                    .cornerRadius(5)
-                
-                Spacer().frame(maxHeight: screenHeight * 0.01)
-                
-                // Field: Academic Level
-                Text("التحصيل الحوزوي او الدراسي ( مع التخصص الدقيق)")
-                    .alignmentGuide(.leading) { d in d[.trailing] } // Custom alignment
-                    .frame(maxWidth: .infinity, alignment: .trailing) // Push
-                    .font(.custom("BahijTheSansArabic-Bold", size: UIDevice.current.userInterfaceIdiom == .phone ? screenWidth * 0.032 : screenWidth * 0.02))
-                    .padding(.trailing, UIDevice.current.userInterfaceIdiom == .pad ? screenWidth * 0.2 : screenWidth * 0.05)
-                
-                TextField("", text: $academicLevel)
-                    .frame(maxWidth: screenHeight * 0.4)
-                    .frame(height: screenHeight * 0.05)
-                    .multilineTextAlignment(.trailing)
-                    .padding(.horizontal)
-                    .background(Color.white)
-                    .cornerRadius(5)
-                    .focused($activeField, equals: .academicLevel) // Focus on this field
-                
-                Spacer().frame(maxHeight: screenHeight * 0.01)
-                
-                // Field: Current Work
-                Text("العمل الحالي")
-                    .alignmentGuide(.leading) { d in d[.trailing] } // Custom alignment
-                    .frame(maxWidth: .infinity, alignment: .trailing) // Push
-                    .font(.custom("BahijTheSansArabic-Bold", size: UIDevice.current.userInterfaceIdiom == .phone ? screenWidth * 0.032 : screenWidth * 0.02))
-                    .padding(.trailing, UIDevice.current.userInterfaceIdiom == .pad ? screenWidth * 0.2 : screenWidth * 0.05)
-                
-                TextField("", text: $currentWork)
-                    .frame(maxWidth: screenHeight * 0.4)
-                    .frame(height: screenHeight * 0.05)
-                    .multilineTextAlignment(.trailing)
-                    .padding(.horizontal)
-                    .background(Color.white)
-                    .cornerRadius(5)
-                    .focused($activeField, equals: .currentWork) // Focus on this field
-                
-                Spacer().frame(maxHeight: screenHeight * 0.01)
-                
-                // Field: City Number
-                Text("رمز المحافظة")
-                    .alignmentGuide(.leading) { d in d[.trailing] } // Custom alignment
-                    .frame(maxWidth: .infinity, alignment: .trailing) // Push
-                    .font(.custom("BahijTheSansArabic-Bold", size: UIDevice.current.userInterfaceIdiom == .phone ? screenWidth * 0.032 : screenWidth * 0.02))
-                    .padding(.trailing, UIDevice.current.userInterfaceIdiom == .pad ? screenWidth * 0.2 : screenWidth * 0.05)
-                
-                TextField("", text: $cityNumber)
-                    .frame(maxWidth: screenHeight * 0.4)
-                    .frame(height: screenHeight * 0.05)
-                    .multilineTextAlignment(.trailing)
-                    .padding(.horizontal)
-                    .background(Color.white)
-                    .cornerRadius(5)
-                    .focused($activeField, equals: .cityNumber) // Focus on this field
-                
-            }
-            .padding(.horizontal, UIScreen.main.bounds.width < 500 ? 16 : 0)
-            .offset(y: calculateOffset()) // Apply offset only for the bottom fields
-            .onAppear {
-                self.addKeyboardObservers() // Start observing keyboard
-            }
-            .onDisappear {
-                self.removeKeyboardObservers() // Stop observing keyboard when the view disappears
             }
             
-            VStack(spacing: 0){
-                PreviousNextButton(previousAction: {}, nextAction: {}, geoW: screenWidth, geoH: screenHeight, isNextNavigating: true, isPreviosNavigating: true, destination: AnyView(EmptyView()), destinationBack: AnyView(LoginPageWelcom()) , color: Color.white, imageName: "Group 9")
-            }.offset(y: screenHeight * 0.04)
-        }
-        .padding(UIScreen.main.bounds.width < 400 ? 16 : 0)
-        .padding()
-        .frame(maxWidth: .infinity, maxHeight: .infinity)
-        .background(Color(red: 236/255, green: 242/255, blue: 245/255))
-        .overlay{
-            LogoIUserInfo()
-                .offset(y: UIDevice.current.userInterfaceIdiom == .phone ? screenHeight * 0.01 : screenHeight * 0.02)
-        }
-        .ignoresSafeArea(edges: .bottom) // Ignore bottom safe area
-        .sheet(isPresented: $isPickerVisible) {
-            DatePicker("Select Date", selection: $selectedDate, displayedComponents: .date)
-                .datePickerStyle(WheelDatePickerStyle())
-                .toolbar {
-                    ToolbarItem(placement: .navigationBarTrailing) {
-                        Button("Done") {
-                            isPickerVisible = false
+            .padding(UIScreen.main.bounds.width < 400 ? 16 : 0)
+            .padding()
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+            .background(Color(red: 236/255, green: 242/255, blue: 245/255))
+            .overlay{
+                LogoIUserInfo()
+                    .offset(y: UIDevice.current.userInterfaceIdiom == .phone ? screenHeight * 0.01 : screenHeight * 0.02)
+            }
+            .ignoresSafeArea(edges: .bottom)
+            
+            
+            
+            
+            
+            .sheet(isPresented: $isPickerVisible) {
+                DatePicker("Select Date", selection: $selectedDate, displayedComponents: .date)
+                    .datePickerStyle(WheelDatePickerStyle())
+                    .toolbar {
+                        ToolbarItem(placement: .navigationBarTrailing) {
+                            Button("Done") {
+                                isPickerVisible = false
+                            }
                         }
                     }
-                }
-        }
+            }
+            
+            
+            PreviousNextButton(previousAction: {}, nextAction: {}, geoW: screenWidth, geoH: screenHeight, isNextNavigating: true, isPreviosNavigating: true, destination: AnyView(EmptyView()), destinationBack: AnyView(LoginPageWelcom()) , color: Color.white, imageName: "Group 9")
+         
+                .offset(y: screenHeight * 0.4)
+            
+            
+        }.navigationBarBackButtonHidden(true)
+        
+        
+        
+       
+            
+      
     }
+
     
     // Function to format date to a readable string
     private func formattedDate(_ date: Date) -> String {
@@ -194,7 +225,10 @@ struct registerPage: View {
     
     // Calculate offset based on keyboard height
     private func calculateOffset() -> CGFloat {
-        return keyboardHeight > 0 ? -keyboardHeight + 40 : 0 // Adjust the offset value
+        if [.academicLevel, .currentWork, .cityNumber].contains(activeField) {
+            return keyboardHeight > 0 ? -keyboardHeight + 40 : 0
+        }
+        return 0
     }
     
     // Add keyboard observers
@@ -215,13 +249,9 @@ struct registerPage: View {
     }
 }
 
-    
-    
-    
-    #Preview {
-        registerPage()
-    }
-    
+#Preview {
+    registerPage()
+}
     
 
 
