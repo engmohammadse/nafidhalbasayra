e5798adbf772fbf9e8c461ab0784880333f3df50
mohammad <mohammadalaaraji@gmail.com>
2024-10-08 15:23:36 +0300

    update registerPage1

--- nafidhalbasayra/View/LoginFolder/registerPage.swift
+++ nafidhalbasayra/View/LoginFolder/registerPage.swift
@@ -230,7 +230,7 @@ struct registerPage: View {
             .overlay{
                 
                 
-                PreviousNextButton(previousAction: {}, nextAction: {}, geoW: screenWidth, geoH: screenHeight, isNextNavigating: true, isPreviosNavigating: true, destination: AnyView(ContentView1()), destinationBack: AnyView(LoginPageWelcom()) , color: Color.white, imageName: "Group 9")
+                PreviousNextButton(previousAction: {}, nextAction: {}, geoW: screenWidth, geoH: screenHeight, isNextNavigating: true, isPreviosNavigating: true, destination: AnyView(registerPage1()), destinationBack: AnyView(LoginPageWelcom()) , color: Color.white, imageName: "Group 9")
              
                     .offset(y: UIScreen.main.bounds.width < 400 ? screenHeight * 0.6 : screenHeight * 0.4)
                                     
