1446c1458bc78701a845b941f0f2cd2adf714878
mohammad <mohammadalaaraji@gmail.com>
2024-10-16 19:59:26 +0300

    Update AttendanceHistorySection

--- nafidhalbasayra/View/MainViewPage/AttendanceHistorySection.swift
+++ nafidhalbasayra/View/MainViewPage/AttendanceHistorySection.swift
@@ -14,115 +14,12 @@ struct AttendanceHistorySection: View {
             
             ScrollView{
                 
-              
-                VStack {
-                    Color.clear
-                       
-                    
-                    VStack {
-                            
-                            HStack{
-                                
-                                VStack{
-                                    Text("المرحلة")
-                                        .font(.custom("BahijTheSansArabic-Bold", size: uiDevicePhone ? screenWidth * 0.04 : screenWidth * 0.023 ))
-                                    Spacer()
-                                        
-                                        .frame(height: screenHeight * 0.02)
-                                    Text("الاولى")
-                                        .font(.custom("BahijTheSansArabic-Plain", size: uiDevicePhone ? screenWidth * 0.04 : screenWidth * 0.023 ))
-                                    
-                                    
-                                }
-                                Spacer()
-                                    .frame(width: screenWidth * 0.2)
-                                
-                                VStack{
-                                    Text("العمر")
-                                        .font(.custom("BahijTheSansArabic-Bold", size: uiDevicePhone ? screenWidth * 0.04 : screenWidth * 0.023 ))
-                                    Spacer()
-                                        .frame(height: screenHeight * 0.02)
-                                    Text("١٢ سنة")
-                                        .font(.custom("BahijTheSansArabic-Plain", size: uiDevicePhone ? screenWidth * 0.04 : screenWidth * 0.023 ))
-                                }
-                                
-                                
-                                Spacer()
-                                    .frame(width: screenWidth * 0.2)
-                                
-                                VStack{
-                                    Text("اسم الطالب")
-                                        .font(.custom("BahijTheSansArabic-Bold", size: uiDevicePhone ? screenWidth * 0.04 : screenWidth * 0.023 ))
-                                    Spacer()
-                                        .frame(height: screenHeight * 0.02)
-                                    Text("احمد")
-                                        .font(.custom("BahijTheSansArabic-Plain", size: uiDevicePhone ? screenWidth * 0.04 : screenWidth * 0.023 ))
-                                }
-         
-                            }
-                            
-                            HStack(spacing: 0) {
-                                       // First half with red background
-                                       ZStack {
-                                           Color(red: 228/255, green: 194/255, blue: 194/255)
-                                           
-                                           
-                                           Button(action: {}){
-                                               Text("حذف بيانات الطالب")
-                                                   .font(.custom("BahijTheSansArabic-Bold", size: uiDevicePhone ? screenWidth * 0.04 : screenWidth * 0.023 ))
-                                                   .foregroundColor(Color(red: 123/255, green: 42/255, blue: 42/255))
-                                                   .padding(.all, screenWidth * 0.02)
-                                           }
-                                           
-                                       }
-                                       
-                                       .frame(maxWidth: .infinity) // Takes up half of the HStack width
-                                
-                                       
-                                       // Second half with blue background
-                                       ZStack {
-                                           Color(red: 223/255, green: 239/255, blue: 242/255)
-                                           
-                                           Button(action: {}){
-                                               Text("تعديل البيانات")
-                                                   .font(.custom("BahijTheSansArabic-Bold", size: uiDevicePhone ? screenWidth * 0.04 : screenWidth * 0.023 ))
-                                                   .foregroundColor(Color(red: 24/255, green: 82/255, blue: 100/255))
-                                                   .padding(.all, screenWidth * 0.02)
-                                           }
-                                           
-                                       }
-                                     
-                                       .frame(maxWidth: .infinity) // Takes up half of the HStack width
-                                   }
-                                   
-                            
-                            
-                            
-                    }.padding(.top, screenHeight * 0.025)
-                        
-                        
-                           
-                           
-                            
-                            
-                    
-                        .background(Color.white)
-                    .cornerRadius(5)
-                    
-                    
-                    
-                       
-
-                } .overlay{
-                    Image("Group 124")
-                        .resizable()
-                        .aspectRatio(contentMode: .fit)
-                        .frame(width: screenWidth * 0.06)
-                        .offset(y: screenHeight * -0.075)
-                   
-                }
+              Spacer()
+                    .frame(height: screenHeight * 0.05)
             
-                  
+                studentInfo()
+                studentInfo()
+                studentInfo()
                
                  
                 
@@ -130,6 +27,20 @@ struct AttendanceHistorySection: View {
             
         }.padding()
         .background(Color(red: 236 / 255, green: 242 / 255, blue: 245 / 255))
+        .overlay(
+           LogoIUserInfo()
+               .offset(y: UIDevice.current.userInterfaceIdiom == .phone ? screenHeight * 0.0 : screenHeight * 0))
+        .overlay{
+            ZStack{
+                Button(action: {}) {
+                    Image("Group 56")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: uiDevicePhone ? screenWidth * 0.08 : screenWidth * 0.064)
+                }
+                .offset(x: screenWidth * 0.46, y: screenHeight * -0.15)
+            }
+        }
             
         
     }
@@ -137,4 +48,135 @@ struct AttendanceHistorySection: View {
 
 #Preview {
     AttendanceHistorySection()
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+struct studentInfo :View {
+    var body: some View {
+        
+        
+        
+          VStack {
+              Color.clear
+                 
+              
+              VStack {
+                      
+                      HStack{
+                          
+                          VStack{
+                              Text("المرحلة")
+                                  .font(.custom("BahijTheSansArabic-Bold", size: uiDevicePhone ? screenWidth * 0.04 : screenWidth * 0.023 ))
+                              Spacer()
+                                  
+                                  .frame(height: screenHeight * 0.02)
+                              Text("الاولى")
+                                  .font(.custom("BahijTheSansArabic-Plain", size: uiDevicePhone ? screenWidth * 0.04 : screenWidth * 0.023 ))
+                              
+                              
+                          }
+                          Spacer()
+                              .frame(width: screenWidth * 0.2)
+                          
+                          VStack{
+                              Text("العمر")
+                                  .font(.custom("BahijTheSansArabic-Bold", size: uiDevicePhone ? screenWidth * 0.04 : screenWidth * 0.023 ))
+                              Spacer()
+                                  .frame(height: screenHeight * 0.02)
+                              Text("١٢ سنة")
+                                  .font(.custom("BahijTheSansArabic-Plain", size: uiDevicePhone ? screenWidth * 0.04 : screenWidth * 0.023 ))
+                          }
+                          
+                          
+                          Spacer()
+                              .frame(width: screenWidth * 0.2)
+                          
+                          VStack{
+                              Text("اسم الطالب")
+                                  .font(.custom("BahijTheSansArabic-Bold", size: uiDevicePhone ? screenWidth * 0.04 : screenWidth * 0.023 ))
+                              Spacer()
+                                  .frame(height: screenHeight * 0.02)
+                              Text("احمد")
+                                  .font(.custom("BahijTheSansArabic-Plain", size: uiDevicePhone ? screenWidth * 0.04 : screenWidth * 0.023 ))
+                          }
+   
+                      }
+                      
+                      HStack(spacing: 0) {
+                                 // First half with red background
+                                 ZStack {
+                                     Color(red: 228/255, green: 194/255, blue: 194/255)
+                                     
+                                     
+                                     Button(action: {}){
+                                         Text("حذف بيانات الطالب")
+                                             .font(.custom("BahijTheSansArabic-Bold", size: uiDevicePhone ? screenWidth * 0.035 : screenWidth * 0.023 ))
+                                             .foregroundColor(Color(red: 123/255, green: 42/255, blue: 42/255))
+                                             .padding(.all, screenWidth * 0.02)
+                                     }
+                                     
+                                 }
+                                 
+                                 .frame(maxWidth: .infinity) // Takes up half of the HStack width
+                          
+                                 
+                                 // Second half with blue background
+                                 ZStack {
+                                     Color(red: 223/255, green: 239/255, blue: 242/255)
+                                     
+                                     Button(action: {}){
+                                         Text("تعديل البيانات")
+                                             .font(.custom("BahijTheSansArabic-Bold", size: uiDevicePhone ? screenWidth * 0.035 : screenWidth * 0.023 ))
+                                             .foregroundColor(Color(red: 24/255, green: 82/255, blue: 100/255))
+                                             .padding(.all, screenWidth * 0.02)
+                                     }
+                                     
+                                 }
+                               
+                                 .frame(maxWidth: .infinity) // Takes up half of the HStack width
+                             }
+                             
+                      
+                      
+                      
+              }.padding(.top, screenHeight * 0.025)
+                  
+                  
+                     
+                     
+                      
+                      
+              
+                  .background(Color.white)
+              .cornerRadius(5)
+              
+              
+              
+                 
+
+          } .overlay{
+              Image("Group 124")
+                  .resizable()
+                  .aspectRatio(contentMode: .fit)
+                  .frame(width: screenWidth * 0.06)
+                  .offset(y: screenHeight * -0.075)
+             
+          }
+        
+        
+    }
 }
