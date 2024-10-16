a0babce1f696d4f97ed801b23dc4df06b195c3ae
mohammad <mohammadalaaraji@gmail.com>
2024-10-16 15:08:45 +0300

    Update NotificationChannelSection

--- nafidhalbasayra/View/MainViewPage/NotificationChannelSection.swift
+++ nafidhalbasayra/View/MainViewPage/NotificationChannelSection.swift
@@ -13,30 +13,43 @@ struct NotificationChannelSection: View {
         VStack{
             ScrollView{
                 
-                VStack {
-                    HStack{
-                        Spacer()
-                        Text("عنوان الرسالة")
-                        
-                    }
-                    Spacer()
-                        .frame(height: screenHeight * 0.03)
-                    
-                   Text("وقد شهدت أروقة جامعة القادسيّة أولى هذه الجلسات التي انعقدت في الأقسام الداخلية بحضور عدد كبير من الطلبة، إذ قدّم المحاضرة الشيخ حيدر اليونسيّ، متناولًا فيها أهمّيّة التغيير الإيجابيّ، وضرورة اتّباع السلوك القويم المتوافق مع مبادئ الشريعة الإسلاميّة . وقد شهدت أروقة جامعة القادسيّة أولى هذه الجلسات التي انعقدت في الأقسام الداخلية بحضور عدد كبير من الطلبة، إذ قدّم المحاضرة الشيخ حيدر اليونسيّ، متناولًا فيها أهمّيّة التغيير الإيجابيّ، وضرورة اتّباع السلوك القويم المتوافق مع مبادئ الشريعة الإسلاميّة ." )
-                        
-                    
-                }
+                Spacer()
+                    .frame(height: screenHeight * 0.1)
                 
-                .padding(screenWidth * 0.09)
-                    .multilineTextAlignment(.trailing)
-                    .background(Color.white)
-                    .cornerRadius(5)
-                    
+                message(titleMessage:  "عنوان الرسالة", bodyMessage:  "وقد شهدت أروقة جامعة القادسيّة أولى هذه الجلسات التي انعقدت في  مبادئ الشريعة الإسلاميّة .", MessageSender:  "مرسل الرسالة", dateMessage: "2024/4/16  10:42 AM")
+                
+                Spacer()
+                    .frame(height: screenHeight * 0.05)
+                
+                message(titleMessage:  "عنوان الرسالة", bodyMessage:  "وقد شهدت أروقة جامعة القادسيّة أولى هذه الجلسات التي انعقدت في الأقسام الداخلية بحضور عدد كبير من الطلبة، إذ قدّم المحاضرة إذ قدّم المحاضرة الشيخ حيدر اليونسيّ، متناولًا فيها أهمّيّة التغيير الإيجابيّ، وضرورة اتّباع السلوك القويم المتوافق مع مبادئ الشريعة الإسلاميّة .", MessageSender:  "مرسل الرسالة", dateMessage: "2024/4/16  10:42 AM")
+                
+                Spacer()
+                    .frame(height: screenHeight * 0.05)
+                
+                message(titleMessage:  "عنوان الرسالة", bodyMessage:  "وقد شهدت أروقة جامعة القادسيّة أولى هذه الجلسات التي انعقدت في  مبادئ الشريعة الإسلاميّة .", MessageSender:  "مرسل الرسالة", dateMessage: "2024/4/16  10:42 AM")
+
+                
                 
                 
             }
         }
+        .padding(.horizontal, screenWidth * 0.1)
+           
         .background(Color(red: 236 / 255, green: 242 / 255, blue: 245 / 255))
+        .overlay(
+           LogoIUserInfo()
+               .offset(y: UIDevice.current.userInterfaceIdiom == .phone ? screenHeight * 0.0 : screenHeight * 0))
+        .overlay{
+            ZStack{
+                Button(action: {}) {
+                    Image("Group 56")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: uiDevicePhone ? screenWidth * 0.08 : screenWidth * 0.064)
+                }
+                .offset(x: screenWidth * 0.46, y: screenHeight * -0.15)
+            }
+        }
         
         
     }
@@ -45,3 +58,60 @@ struct NotificationChannelSection: View {
 #Preview {
     NotificationChannelSection()
 }
+
+
+struct message: View {
+    @State  var titleMessage: String
+    @State  var bodyMessage: String
+    @State  var MessageSender: String
+    @State  var dateMessage: String
+    
+    var body: some View {
+        
+        VStack {
+        
+            HStack{
+                Spacer()
+                Text(titleMessage)
+                    .font(.custom("BahijTheSansArabic-Bold", size: uiDevicePhone ? screenWidth * 0.035 : screenWidth * 0.023 ))
+              
+        
+            }
+            Spacer()
+                .frame(height: screenHeight * 0.03)
+        
+           Text(bodyMessage)
+                .font(.custom("BahijTheSansArabic-Bold", size: uiDevicePhone ? screenWidth * 0.03 : screenWidth * 0.023 ))
+          
+        
+            Spacer()
+                .frame(height: screenHeight * 0.03)
+        
+            HStack {
+                Spacer()
+                Text(MessageSender)
+                    .font(.custom("BahijTheSansArabic-Bold", size: uiDevicePhone ? screenWidth * 0.035 : screenWidth * 0.023 ))
+               
+            }
+        
+        
+            HStack {
+        
+                Text(dateMessage)
+                    .font(.custom("BahijTheSansArabic-Bold", size: uiDevicePhone ? screenWidth * 0.03 : screenWidth * 0.023 ))
+               
+                Spacer()
+            }
+            .offset(x: screenWidth * -0.05, y: screenHeight * 0.01)
+        }
+        
+        .padding(.horizontal,screenWidth * 0.09)
+        .padding(.vertical,screenHeight * 0.025)
+            .multilineTextAlignment(.trailing)
+            .background(Color.white)
+            .cornerRadius(5)
+        
+    }
+}
+
+    
